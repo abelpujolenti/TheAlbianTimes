0912cdc0e5803f28a831337bb65d3bbd75e81748
adri2711 <adri271101@gmail.com>
2023-10-30 22:25:36 +0100

    country & countryevent

---
+++ TheAlbianTimes/Assets/Scripts/Countries/Country.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Country : MonoBehaviour
+{
+    
+    public float reputation = .5f;
+    public float censorship = 0f;
+
+    [SerializeField] protected AnimationCurve censorshipCurve;
+    [SerializeField] protected AnimationCurve giftCurve;
+    [SerializeField] protected AnimationCurve bribeCurve;
+    [SerializeField] protected AnimationCurve threatCurve;
+    [SerializeField] protected string[] events;
+
+    private static CountryEvent[] countryEvents;
+    private float lastReputationChange = 0f;
+
+    private void Start()
+    {
+        Setup();
+    }
+
+    protected virtual void Setup()
+    {
+        LoadCountryEvents();
+    }
+
+    public void AffectReputation(float change)
+    {
+        float prevRep = reputation;
+        reputation = Mathf.Max(0f, Mathf.Min(1f, reputation + change));
+        lastReputationChange += reputation - prevRep;
+
+        censorship = censorshipCurve.Evaluate(reputation);
+    }
+
+    public virtual CountryEvent GenerateEvent()
+    {
+        return null;
+    }
+
+    protected void LoadCountryEvents()
+    {
+
+    }
+    protected static CountryEvent[] GetCountryEvents()
+    {
+        if (countryEvents == null)
+        {
+            System.Object[] files = Resources.LoadAll("Json/CountryEvents", typeof(TextAsset));
+            Array.Resize(ref countryEvents, files.Length);
+            for (int i = 0; i < countryEvents.Length; i++)
+            {
+                countryEvents[i] = JsonUtility.FromJson<CountryEvent>(((TextAsset)files[i]).text);
+                Debug.Log(countryEvents[i].id);
+            }
+        }
+        return countryEvents;
+    }
+}

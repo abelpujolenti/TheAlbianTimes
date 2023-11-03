dfafcd79c3622c1cbb7415c0aa6b5bd25ef90974
Abel Pujol <abelpujol99@gmail.com>
2023-11-03 22:30:03 +0100

    LayoutManagerData

--- TheAlbianTimes/Assets/Scripts/Managers/GameManager.cs
+++ TheAlbianTimes/Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,3 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Managers
@@ -11,8 +9,6 @@ namespace Managers
         public static GameManager Instance => _instance;
 
         private LayoutManager _layoutManager;
-
-        private float counter = 5;
 
         private void Awake()
         {
@@ -26,18 +22,6 @@ namespace Managers
                 Destroy(gameObject);
             }
         }
-
-        /*private void Update()
-        {
-            counter -= Time.deltaTime;
-
-            if (counter < 0f)
-            {
-                LayoutManager obj = Instantiate(_layoutManager);
-                //obj.AddComponent<LayoutManager>();
-                counter = 10;
-            }
-        }*/
 
         public void SetLayoutManager(LayoutManager layoutManager)
         {

aa718ba2b3cabf508cc741bef811a6b48a144fc2
adri2711 <adri271101@gmail.com>
2023-11-16 03:32:11 +0100

    ok

--- TheAlbianTimes/Assets/Scripts/Utility/InteractableRectTransform.cs
+++ TheAlbianTimes/Assets/Scripts/Utility/InteractableRectTransform.cs
@@ -100,7 +100,6 @@ namespace Utility
         
         protected virtual void BeginDragCamera(BaseEventData data)
         {
-            Debug.Log("beginmovecam");
             PointerEventData pointerData = (PointerEventData) data;            
 
             _initialMouseXPosition = Camera.main.ScreenToWorldPoint(pointerData.position).x;
@@ -110,7 +109,6 @@ namespace Utility
 
         protected virtual void DragCamera(BaseEventData data)
         {
-            Debug.Log("movecam");
             if (!held && !moveCamera) return;
 
             PointerEventData pointerData = (PointerEventData) data;

6f95768f43394c6ed00c185c657f67b179222691
adri2711 <adri271101@gmail.com>
2023-11-26 20:48:43 +0100

    start animation

--- TheAlbianTimes/Assets/Scripts/MainMenu/PlayButton.cs
+++ TheAlbianTimes/Assets/Scripts/MainMenu/PlayButton.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 
 public class PlayButton : MonoBehaviour
 {
+    [SerializeField] Animator backgroundAnimator;
+    [SerializeField] Animator buttonAnimator;
+    private Coroutine startGameCoroutine;
     public void OnClick()
     {
+        if (startGameCoroutine != null) return;
+        startGameCoroutine = StartCoroutine(StartGameCoroutine());
+    }
+
+    private IEnumerator StartGameCoroutine()
+    {
+        backgroundAnimator.Play("Start", 0);
+        buttonAnimator.Play("ButtonsFadeOut", 0);
+        yield return new WaitForFixedUpdate();
+        AnimatorClipInfo[] currentClipInfo = backgroundAnimator.GetCurrentAnimatorClipInfo(0);
+        yield return new WaitForSeconds(currentClipInfo[0].clip.length);
+        yield return new WaitForSeconds(.5f);
         GameManager.Instance.sceneLoader.SetScene("WorkspaceScene");
     }
 }

27e58e85712f70ef0d979d8256e569f902dcf75f
abel pujol <abel.pujol@enti.cat>
2024-02-19 12:15:57 +0100

    Go to home

---
+++ TheAlbianTimes/Assets/Scripts/Workspace/Notebook/Pages/Country/NotebookCountryPage0.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NotebookCountryPage0 : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _countryName;
+    [SerializeField] private Sprite _flagImage;
+    [SerializeField] private TextMeshProUGUI _description;
+    [SerializeField] private TextMeshProUGUI _history;
+    
+    
+}

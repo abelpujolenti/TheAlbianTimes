dede29c96a9b0a90c8a4e7a057970c4e2297015a
adri <40923191+adri2711@users.noreply.github.com>
2024-05-10 23:26:00 +0200

    missing parentheses

--- TheAlbianTimes/Assets/Scripts/Stats/StaffSheet.cs
+++ TheAlbianTimes/Assets/Scripts/Stats/StaffSheet.cs
@@ -57,7 +57,7 @@ namespace Stats
         }
         private void UpdateCost()
         {
-            string text = "<color=red>Cost: " + (staffCost * GameManager.Instance.gameState.playerData.staff + staffChange).ToString("F2") + " / day</color>";
+            string text = "<color=red>Cost: " + (staffCost * (GameManager.Instance.gameState.playerData.staff + staffChange)).ToString("F2") + " / day</color>";
             cost.text = text;
         }
         public float UpdateStaffCost()
